2f1d398e124ebfef7d3c91595be752e4940623a0
HuaWei <24174126@qq.com>
2021-06-08 03:06:13 +0800

    create proj with examples

---
+++ iStyleGuideExamples/iStyleGuideExamples/MyType+MyProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MyType+MyProtocol.swift
+//  iStyleGuideExamples
+//
+//  Created by Huawei on 2021/6/8.
+//
+
+import Foundation
+
+protocol MyTypeProtocal {
+    func myProtocalFunc() -> Void
+}
---
+++ iStyleGuideExamples/iStyleGuideExamples/MyType.swift
@@ -0,0 +1,15 @@
+//
+//  MyType.swift
+//  iStyleGuideExamples
+//
+//  Created by Huawei on 2021/6/8.
+//
+
+import UIKit
+
+/**
+ * 文件命名
+ */
+class MyType {
+    var myName: String = "Sam"
+}
